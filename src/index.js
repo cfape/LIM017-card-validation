fd4cdffaa3897b4af06ee9fc93c44a37d8b1e65e
cfape <c.penagos16@gmail.com>
2022-02-03 23:31:28 -0500

    actualizar

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 // Comando para validator
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
 
-var creditCardNumber =[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p];
-//function nroCard (cardNumber) {
-    //console.log (`${cardNumber}`);
-    //for (var i = 0; i <=9; creditCardNumber.length; i++){
-    //    nroCard(cardNumber[i]);
-    //}
 
-    
+
+
 
