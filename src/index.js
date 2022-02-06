8787de876e9f32656725db7888ecde22f2a47d3a
cfape <c.penagos16@gmail.com>
2022-02-05 20:48:32 -0500

    actualizar

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@
 
 //console.log(validator);
 
-const numeroDeTarjeta = document.getElementById("inputNumCard").value;
-
+const numeroDeTarjeta = [a, b, c, d, e, f, g, h,i, j, k, l, m, n, o, p];
+for (var i = 0; i < numeroDeTarjeta.length; i++) {
+    console.log(numeroDeTarjeta[i]);
+}
+numeroDeTarjeta = document.getElementById("inputNumCard").value;
 console.log("inputNumCard");
+
 
 
 
