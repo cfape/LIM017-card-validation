d5c5294465c5c9e2f673e58a589c515c75622b20
cfape <c.penagos16@gmail.com>
2022-02-07 22:50:00 -0500

    modificaciones

--- src/index.js
+++ src/index.js
@@ -1,28 +1,17 @@
-// Comando para validator
-//import validator from './validator.js';
+//Comando para validator
+import validator from './validator.js';
+//Llamando al DOM
+let buttonNumberCard = document.getElementById('btn');
+//Captura valores del input
+buttonNumberCard.addEventListener('click',clickButton);
+
+function clickButton() {
+    let creditCardNumber = document.getElementById('numberCard').value;
+    validator.isValid(creditCardNumber);
+
+}
 
 //console.log(validator);
-
-//Obtener número de tarjeta
-var numeroDeTarjeta1 = [number1, number2,number3,number4,number5,number6,number7,number8,number9,number10,number11,number12,number13,number14,number15,number16];
-
-
-
-/*let btn2 = document.getElementById('btn2');
-btn2.addEventListener('click', (playbtn);
-
-function botonValidar (inputNumCard){
-    let inputNumCard = document.getElementById('inputNumCard.value');
-        inputNumCard = inputNumCard.value;
-    for (let i = 0; i <= 9; i++){
-    }
-    }
-    console.log(number(inputNumCard.value));
-    /*
-    let verificar = validator.isValid(inputNumCard)
-        console.log(verificar);
-    }
-
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,28 @@
 const validator = {
-  // ...
-};
+  isValid: function (creditCardNumber){
+    let arrayNum = Array.from(creditCardNumber);
+    let arrayNum2 = [];
+    let sumNum = 0;
+    for (let i = 0; i < arrayNum.length; i++){
+      arrayNum2.push(parseInt(arrayNum[i],));
+    }
+    arrayNum2.reverse();
+    for (let i = 0; i < arrayNum2.length; i++){
+      if ((i+1)%2==0){
+        arrayNum2 [i]*=2;
+        if(arrayNum2[i]>=10){
+          arrayNum2 [i]-=9;
+        }
+    }
+    }
+    //Retorna si es valida
+    return sumNum %10 === 0;
+    }
+    }
+
+
+
+
+
 
 export default validator;
