3b97f8719c1dbb5e347f0271b3f9143b8c489efd
cfape <c.penagos16@gmail.com>
2022-02-17 12:33:37 -0500

    primera parte algoritmo luhm

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import validator from './validator.js';
 
+
 let validButton = document.getElementById('validButton');
 validButton.addEventListener('click', () => {
+let creditCardNumber = document.getElementById('cardNumber').value;
 
-//console.log(numberCard);
-});
-
-console.log (validator);
+    console.log(creditCardNumber);
+    let message = document.getElementById('mensaje');
+    if (creditCardNumber === "") {
+        message.innerText = '*Por favor ingresa tu número de tarjeta.';
+        message.classList.add('error-text');
+    } else {
+        validator.isValid(creditCardNumber);
+        message.innerText = "";
+        message.classList.remove('error-text');
+        }
+    });
+//console.log (validator);
 
 
 
--- src/validator.js
+++ src/validator.js
@@ -1,19 +1,32 @@
-
 //Objeto validator contiene 2 funciones
 const validator = {
     isValid: function(creditCardNumber) {
-    let arrayNum1 = Array.from(creditCardNumber)
-    //Nuevo array de números de tarjeta
-    let arrayNum2 = []
-    //let totalSum = 0
-    for (let i = 0; i < arrayNum1.length; i++) {
-    arrayNum2.push(arrayNum1[i]);
-    //console.log('arrayNum2');
-    }
+        let array1 = creditCardNumber.split("");
+        //console.log(array1);
+        let reverseArray = array1.reverse();
+        //console.log(reverseArray);
+        let array2 = [];
+        for (let i = 0; i < reverseArray.length; i++) {
+            console.log(i+1);
+            if ((i+1) %2 === 0){              //posición par
+                let parMulti = reverseArray[i+1]*2;
+                if (parMulti >= 10) {
+                let numNew = parMulti - 9;
+                array2.push(numNew);
+                } else {
+                array2.push(parMulti);
+                }
+            } else {                           //posición impar
+                let imparNum = reverseArray[i]
+                array2.push(imparNum);
+            }
+        }
+        //console.log(array2);
+        return creditCardNumber;
     },
-
     maskify: function(creditCardNumber) {
     return creditCardNumber;
-    },
-    }
+}
+}
+//}
 export default validator;
